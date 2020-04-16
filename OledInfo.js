3824b5070dd1612b346ca9daeb87e8714be822b2
360manu <360manu@users.noreply.github.com>
2020-04-16 14:32:12 +0200

    upgrade

--- OledInfo.js
+++ OledInfo.js
@@ -35,39 +35,109 @@ class Oled {
 			this.oled.clearDisplay();
 			this.oled.turnOnDisplay();
 
-			this.oled.drawPixel([
-					[SIZE_X - 1, 0, 1],
-					[SIZE_X - 1, SIZE_Y - 1, 1],
-					[0, SIZE_Y - 1, 1],
-					[0, 0, 1]
-				]);
+			// set the refresh
+			this.shouldUpdate = true;
+			this.screenStatus = 0;
+			this.usb = 'waiting';
+			this.ble = 'waiting';
+			this.gear = -1;
+			this.grade = 0;
 
-			this.oled.drawLine(1, 1, SIZE_X - 2, 1, 1);
-			this.oled.drawLine(SIZE_X - 2, 1, SIZE_X - 2, SIZE_Y - 2, 1);
-			this.oled.drawLine(SIZE_X - 2, SIZE_Y - 2, 1, SIZE_Y - 2, 1);
-			this.oled.drawLine(1, SIZE_Y - 2, 1, 1, 1);
+			// run async
+			this.displayLoop();
+
 		} catch (err) {
 			// Print an error message a
 			console.log(err.message);
 		}
 	};
 
+	displayLoop() {
+		if (this.shouldUpdate == true) {
+			this.shouldUpdate = false;
+			this.Draw().then(() => setTimeout(() => this.displayLoop(), 5000));
+		} else {
+			setTimeout(() => this.displayLoop(), 2000);
+		}
+	};
+
+	async Draw() {
+		this.oled.clearDisplay(false);
+		this.oled.drawPixel([
+				[SIZE_X - 1, 0, 1],
+				[SIZE_X - 1, SIZE_Y - 1, 1],
+				[0, SIZE_Y - 1, 1],
+				[0, 0, 1]
+			], false);
+
+		this.oled.drawLine(1, 1, SIZE_X - 2, 1, 1, false);
+		this.oled.drawLine(SIZE_X - 2, 1, SIZE_X - 2, SIZE_Y - 2, 1, false);
+		this.oled.drawLine(SIZE_X - 2, SIZE_Y - 2, 1, SIZE_Y - 2, 1, false);
+		this.oled.drawLine(1, SIZE_Y - 2, 1, 1, 1, false);
+
+		// reste du message
+		switch (this.screenStatus) {
+		case 0:
+			this.displayStatus();
+			break;
+		case 1:
+			this.displaySimInfo()
+			break;
+		}
+	}
+
+	// Gear
+	displayStatus() {
+		this.oled.setCursor(10, 5);
+		this.oled.writeString(font, 1, 'Status', 1, false);
+		this.oled.setCursor(10, 20);
+		this.oled.writeString(font, 1, 'USB ' + this.usb, 1, false);
+		this.oled.setCursor(10, 38);
+		this.oled.writeString(font, 1, 'BLE ' + this.ble, 1, true);
+	}
+
+	// Gear
+	displaySimInfo() {
+		this.oled.setCursor(80, 5);
+		this.oled.writeString(font, 1, 'Gear', 1, false);
+		this.oled.setCursor(90, 25);
+		this.oled.writeString(font, 2, this.gear.toString(), 1, false);
+		this.oled.setCursor(20, 5);
+		this.oled.writeString(font, 1, 'pente', 1, false);
+		this.oled.setCursor(10, 25);
+		this.oled.writeString(font, 2, this.grade.toString() + '%', 1, true);
+	}
+
+	setStatus(status) {
+		this.screenStatus = status;
+		this.shouldUpdate = true;
+	}
+
+	// iusb
+	displayUSB(message) {
+		this.shouldUpdate = true;
+		this.usb = message;
+	}
+	// iusb
+	displayBLE(message) {
+		this.shouldUpdate = true;
+		this.ble = message;
+	}
+
 	// Gear
 	displayGear(gear) {
-		// Location fits 128x64 OLED
-		this.oled.setCursor(80, 5);
-		this.oled.writeString(font, 1, 'Gear', 1, true);
-		this.oled.setCursor(80, 20);
-		this.oled.writeString(font, 2, gear.toString(), 1, true);
+		if (gear != this.gear) {
+			this.shouldUpdate = true;
+			this.gear = gear;
+		}
 	}
 
 	// Gear
 	displayGrade(grade) {
-		// Location fits 128x64 OLED
-		this.oled.setCursor(20, 5);
-		this.oled.writeString(font, 1, 'pente', 1, true);
-		this.oled.setCursor(20, 20);
-		this.oled.writeString(font, 2, grade.toString() + '%', 1, true);
+		if (grade != this.grade) {
+			this.shouldUpdate = true;
+			this.grade = grade;
+		}
 	}
 }
 module.exports = Oled
--- server.js
+++ server.js
@@ -1,28 +1,35 @@
+var util = require('util');
+var fs = require('fs');
+var ws = fs.createWriteStream('./Ketter.log', {
+		flags: 'a'
+	});
+var through = require('through2');
+var t = new through();
+t.pipe(process.stdout);
+t.pipe(ws);
+console.log = function () {
+	t.write(util.format.apply(this, arguments) + '\n');
+};
+
+var DEBUG = false;
 var express = require('express');
 var kettlerUSB = require('./kettlerUSB');
 var KettlerBLE = require('./BLE/kettlerBLE');
-var DEBUG = false;
 var BikeState = require('./BikeState');
 var Oled = require('./OledInfo');
+var Button = require('./lib/rpi_gpio_buttons');
 
-/* Web Server on port 3000 for inspecting the Kettler State */
+//--- Web Server on port 3000 for inspecting the Kettler State
 const app = express();
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
-app.get('/', function (req, res) { 	res.render('index'); });
-server = app.listen(3000, function () { console.log('Kettler app listening on port 3000!'); });
-
-// socket.io instantiation
+app.get('/', function (req, res) {
+	res.render('index');
+});
+server = app.listen(3000, function () {
+		console.log('Kettler app listening on port 3000!');
+	});
 const io = require("socket.io")(server);
-var kettlerUSB = new kettlerUSB();
-var kettlerBLE = new KettlerBLE(serverCallback);
-var bikeState = new BikeState();
-var oled = new Oled();
-
-// go
-var kettlerObs = kettlerUSB.open();
-
-// Web server callback
 io.on('connection', (socket) => {
 	socket.on('key', function (ev) {
 		console.log('key' + ev);
@@ -45,9 +52,23 @@ io.on('connection', (socket) => {
 		}
 	});
 });
-// end WebServer
 
-// un peu de retour serveur  
+//--- Buttons
+var button = new Button(7);
+button.on('clicked', function () {
+	bikeState.GearUp();
+});
+ var button = new Button(11);
+button.on('clicked', function () {
+	bikeState.GearDown();
+});
+ 
+//--- Oled Screen
+var oled = new Oled();
+
+//--- Machine State
+var bikeState = new BikeState();
+// un peu de retour serveur
 bikeState.on('mode', (mode) => {
 	io.emit('mode', mode);
 });
@@ -65,17 +86,25 @@ bikeState.on('windspeed', (windspeed) => {
 bikeState.on('simpower', (simpower) => {
 	kettlerUSB.setPower(simpower);
 });
+// first state
+bikeState.setGear(4);
 
-/* BRIDGE : Kettler USB infor to BLE peripheral & Webserver */
-kettlerObs.on('error', (string) => {
-	console.log('error' + string);
+//--- Serial port
+var kettlerUSB = new kettlerUSB();
+kettlerUSB.on('error', (string) => {
+	console.log('error : ' + string);
 	io.emit('error', string);
 });
-
-kettlerObs.on('data', (data) => {
+kettlerUSB.on('connecting', () => {
+	oled.displayUSB('connecting');
+});
+kettlerUSB.on('start', () => {
+	oled.displayUSB('connected');
+});
+kettlerUSB.on('data', (data) => {
 	// keep
 	bikeState.setData(data);
-	
+
 	// send to html server
 	if ('speed' in data)
 		io.emit('speed', data.speed.toFixed(1));
@@ -89,10 +118,21 @@ kettlerObs.on('data', (data) => {
 	// send to BLE adapter
 	kettlerBLE.notifyFTMS(data);
 });
+kettlerUSB.open();
 
-// End Bridge
+//--- BLE server
+var kettlerBLE = new KettlerBLE(serverCallback);
 
-// BLE FTMS to Bike & Server
+kettlerBLE.on('advertisingStart', (client) => {
+	oled.displayBLE('Started');
+});
+kettlerBLE.on('accept', (client) => {
+	oled.displayBLE('Connected');
+});
+kettlerBLE.on('disconnect', (client) => {
+	oled.displayBLE('Disconnected');
+});
+
 function serverCallback(message, ...args) {
 	var success = false;
 	switch (message) {
@@ -105,12 +145,13 @@ function serverCallback(message, ...args) {
 
 	case 'control':
 		console.log('[server.js] - Bike is under control');
+		oled.setStatus(1);
 		bikeState.setControl();
 		success = true;
 		break;
 
 	case 'power':
-		console.log('[server.js] - Bike in ERG Mode');
+		// console.log('[server.js] - Bike in ERG Mode');
 		bikeState.setTargetPower(args[0]);
 		success = true;
 		break;
@@ -121,16 +162,41 @@ function serverCallback(message, ...args) {
 		var grade = Number(args[1]);
 		var crr = Number(args[2]);
 		var cw = Number(args[3]);
-		console.log('[server.js] - Bike SIM Mode - [wind]: ' + (windspeed*3.6).toFixed(1) + 'hm/h [grade]: ' + grade.toFixed(1) + '% [crr]: ' + crr + ' [cw]: ' + cw)
+		// console.log('[server.js] - Bike SIM Mode - [wind]: ' + (windspeed * 3.6).toFixed(1) + 'hm/h [grade]: ' + grade.toFixed(1) + '% [crr]: ' + crr + ' [cw]: ' + cw)
 
 		bikeState.setExternalCondition(windspeed, grade, crr, cw);
 		// nothing special
 		success = true;
 		break;
 	}
-
 	return success;
 };
 
-// GO
-kettlerUSB.start();
+/**** TEST ***/
+/*
+setInterval(mafonction, 2000);
+setInterval(mafonction2, 4000);
+oled.setStatus(1);
+
+bikeState.on('simpower', (simpower) => {
+	dataFake.power = simpower;
+});
+var dataFake = {
+	rpm: 80, //+ 20 * (Math.random() - 0.5),
+	speed: 20,
+	power: 100
+};
+function mafonction() {
+	kettlerUSB.emit('data', dataFake);
+
+	dataFake = {
+		rpm: 80 + 20 * (Math.random() - 0.5),
+		speed: 20,
+		power: 100
+	};
+};
+function mafonction2() {
+	var grade = 3; //20 * (Math.random() - 0.5);
+	bikeState.setExternalCondition(0, grade, 0.005, 0.39);
+};
+*/
